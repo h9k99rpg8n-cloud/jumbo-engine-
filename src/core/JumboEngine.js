a17b481e24bcef002978744dbca82e060b46c68f
h9k99rpg8n-cloud <h9k99rpg8n@privaterelay.appleid.com>
2026-05-02 16:19:22 -0600

    Alpha 0.8 conectar nucleo a Babylon

--- src/core/JumboEngine.js
+++ src/core/JumboEngine.js
@@ -1,9 +1,5 @@
-import { Renderer3D } from '../render/Renderer3D.js';
-import { EmptyGridScene } from '../scenes/EmptyGridScene.js';
-import { CameraController } from '../input/CameraController.js';
+import { BabylonRenderer } from '../render/BabylonRenderer.js';
 import { EditorShell } from '../ui/EditorShell.js';
-import { ObjectFactory } from '../objects/ObjectFactory.js';
-import { TransformGizmo } from '../gizmos/TransformGizmo.js';
 
 export class JumboEngine {
   constructor({ contenedor, nombre, version }) {
@@ -12,12 +8,7 @@ export class JumboEngine {
     this.version = version;
 
     this.ui = null;
-    this.renderer3D = null;
-    this.sceneModule = null;
-    this.cameraController = null;
-    this.transformGizmo = null;
-    this.objectFactory = new ObjectFactory();
-    this.animationId = null;
+    this.renderer = null;
   }
 
   iniciar() {
@@ -29,57 +20,13 @@ export class JumboEngine {
 
     this.ui.crear();
 
-    this.renderer3D = new Renderer3D({
+    this.renderer = new BabylonRenderer({
       viewport: this.ui.obtenerViewport(),
     });
 
-    this.sceneModule = new EmptyGridScene({
-      gridSize: 200,
-      gridDivisions: 200,
-    });
-
-    this.sceneModule.crear();
-    this.renderer3D.usarEscena(this.sceneModule.scene);
-
-    this.cameraController = new CameraController({
-      camera: this.renderer3D.camera,
-      domElement: this.renderer3D.domElement,
-      target: this.sceneModule.target,
-      minDistance: 1.5,
-      maxDistance: 240,
-      initialDistance: 70,
-    });
-
-    this.transformGizmo = new TransformGizmo({
-      camera: this.renderer3D.camera,
-      domElement: this.renderer3D.domElement,
-      sceneModule: this.sceneModule,
-      cameraController: this.cameraController,
-    });
-
-    this.cameraController.iniciar();
-    this.transformGizmo.iniciar();
-    this.ui.onCrearCubo(() => this.crearCubo());
-    this.ui.onCambiarHerramienta((tool) => this.transformGizmo.setMode(tool));
-    this.registrarEventos();
-    this.loop();
-  }
-
-  crearCubo() {
-    const cubo = this.objectFactory.crearCubo();
-    this.sceneModule.agregarGameObject(cubo);
-  }
-
-  registrarEventos() {
-    window.addEventListener('resize', () => {
-      this.renderer3D.redimensionar();
-    });
-  }
-
-  loop() {
-    this.animationId = requestAnimationFrame(() => this.loop());
-
-    this.cameraController.actualizar();
-    this.renderer3D.renderizar();
+    this.renderer.setTool('select');
+    this.ui.onCrearCubo(() => this.renderer.crearCubo());
+    this.ui.onCambiarHerramienta((tool) => this.renderer.setTool(tool));
+    this.renderer.renderLoop();
   }
 }
